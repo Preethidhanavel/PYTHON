a5440fb8e1435cec5e0bb216d20841018a4472a9
Preethi D <27preethid@gmail.com>
2025-06-30 22:53:24 +0530

    Add files via upload

---
+++ EXPRESSION/challenge/P01.py
@@ -0,0 +1,5 @@
+#Given a = 4, b = 5, write expressions to calculate their sum and product in a single line.
+a=int(input("Enter the a: "))
+b=int(input("Enter the b: "))
+x,y=a+b,a*b
+print("sum:",x,"product:",y)
